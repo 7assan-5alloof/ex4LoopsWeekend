4c88391f65ac1ffc801989834873c96b2581053a
7assan-5alloof <hassankhalloof20@gmail.com>
2021-04-08 09:12:54 +0400

    Write program

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    for (int number = 1; number < 1000; number++)
+    {
+        // Find Armstrong sum
+        int numberUsed = number;
+        int sum = 0;
+        while (numberUsed > 0)
+        {
+            int digit = numberUsed % 10;
+            sum += pow(digit, 3);
+            numberUsed /= 10;
+        }
+
+        // Compare Armstrong sum with the original number
+        if (sum == number) cout << number << endl;
+    }
+
     return 0;
 }
